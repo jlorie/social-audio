88d0d5e065e6affd98081be114b70116c4f5b682
Javier Lorie Guerra <jlorie87@gmail.com>
2016-05-30 14:49:49 -0500

    Fixed some changes for elements list endpoint

--- services/elements-endpoint/list-elements.js
+++ services/elements-endpoint/list-elements.js
@@ -1,11 +1,33 @@
 import ElementModel from '../commons/resources/element-model';
 
-const URI_ELEMENTS_RESOURCE = process.env.URI_ELEMENTS_RESOURCE;
+const URI_ELEMENTS_BY_USERS = process.env.URI_ELEMENTS_BY_USERS;
 
-const elementModel = new ElementModel(URI_ELEMENTS_RESOURCE);
+const elementModel = new ElementModel(URI_ELEMENTS_BY_USERS);
 export function listElements(userId) {
   console.info('Querying elements for user with id ' + userId);
 
-  let attributes = 'id, created_at, thumbnail_url';
-  return elementModel.get({ userId, attributes });
+  return elementModel.get({ userId })
+    .then(formatResults);
+}
+
+function formatResults(elements) {
+  let result = {
+    items: [],
+    next: elements.next
+  };
+
+  for (let item of elements.items) {
+    let created = item.created_at.split('|');
+
+    result.items.push({
+      id: item.id,
+      created_at: created[0],
+      owner: created[1] === 'owner',
+      audios: item.audios,
+      thumbnail_url: item.thumbnail_url,
+      favorite: item.favorite
+    });
+  }
+
+  return result;
 }
