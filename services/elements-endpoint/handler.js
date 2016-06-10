05b7eb2548c1ff1b2a4971f53a20b175812d5f21
Javier Lorie Guerra <jlorie87@gmail.com>
2016-06-10 12:49:29 -0500

    Fixed issue #13

--- services/elements-endpoint/handler.js
+++ services/elements-endpoint/handler.js
@@ -9,73 +9,74 @@ import { markElementAsViewed } from './mark-notification';
 export default (event, context) => {
   console.info('=> Input: ', JSON.stringify(event, null, 2));
 
-  try {
-    return handleRequest(event)
-      .then(result => {
-        if (event.action === 'list') {
-          console.info('==> Success: ' + result.items.length + ' items listed');
-        } else {
-          console.info('==> Success: ', JSON.stringify(result, null, 2));
-        }
+  return handleRequest(event)
+    .then(result => {
+      if (event.action === 'list') {
+        console.info('==> Success: ' + result.items.length + ' items listed');
+      } else {
+        console.info('==> Success: ', JSON.stringify(result, null, 2));
+      }
 
-        context.succeed(result);
-      })
-      .catch(err => {
-        console.info('==> An error occurred. ', err.stack);
-        throw err;
-      });
-  } catch (err) {
-    let error = {
-      status: 'ERROR',
-      message: err.message
-    };
-    context.fail(JSON.stringify(error));
-  }
+      context.succeed(result);
+    })
+    .catch(err => {
+      console.info('==> An error occurred. ', err.stack);
+      let error = {
+        status: 'ERROR',
+        message: err.message
+      };
+
+      context.fail(JSON.stringify(error));
+    });
 };
 
 function handleRequest(input) {
   let result;
   let userId = input.identity_id.split(':').pop();
 
-  switch (input.action) {
-    case 'list':
-      {
-        result = listElements(userId);
-        break;
-      }
-    case 'detail':
-      {
-        let tasks = [
-          detailElement(input.element_id, userId),
-          markElementAsViewed(input.element_id, userId)
-        ];
-
-        result = Promise.all(tasks).then(results => results[0]);
-        break;
-      }
-    case 'delete':
-      {
-        result = deleteElement(input.element_id, userId);
-        break;
-      }
-    case 'batch_delete':
-      {
-        if (!_.isArray(input.element_ids) || input.element_ids.length === 0) {
-          throw new Error('InvalidParameters');
+  try {
+    switch (input.action) {
+      case 'list':
+        {
+          result = listElements(userId);
+          break;
         }
+      case 'detail':
+        {
+          let tasks = [
+            detailElement(input.element_id, userId),
+            markElementAsViewed(input.element_id, userId)
+          ];
 
-        result = deleteMultipleElements(input.element_ids, userId);
-        break;
-      }
-    case 'detach_audio':
-      {
-        result = detachAudio(input.element_id, input.attachment_id, userId);
-        break;
-      }
-    default:
-      {
-        throw new Error('ActionNotSupported');
-      }
+          result = Promise.all(tasks).then(results => results[0]);
+          break;
+        }
+      case 'delete':
+        {
+          result = deleteElement(input.element_id, userId);
+          break;
+        }
+      case 'batch_delete':
+        {
+          if (!_.isArray(input.element_ids) || input.element_ids.length === 0) {
+            throw new Error('InvalidParameters');
+          }
+
+          result = deleteMultipleElements(input.element_ids, userId);
+          break;
+        }
+      case 'detach_audio':
+        {
+          result = detachAudio(input.element_id, input.attachment_id, userId);
+          break;
+        }
+      default:
+        {
+          throw new Error('ActionNotSupported');
+        }
+    }
+  } catch (err) {
+    return Promise.reject(err);
   }
 
   return result;
