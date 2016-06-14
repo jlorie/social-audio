af0c43d1c55a9c02d3104e22b51d8be3532f2eca
Javier Lorie Guerra <jlorie87@gmail.com>
2016-06-14 17:53:22 -0500

    Implemented issue #21
    activating pending users when register

--- services/commons/constants.js
+++ services/commons/constants.js
@@ -7,3 +7,14 @@ export const NOTIFICATION_TYPE = {
   AUDIO_REQUEST: 'audio_request',
   NEW_AUDIO: 'new_audio'
 };
+
+export const USER_STATUS = {
+  PENDING: 'pending',
+  IDLE: 'idle',
+  ENABLED: 'enabled',
+  DISBLED: 'disabled'
+};
+
+export const ACCOUNT_TYPE = {
+  BASIC: 'basic'
+};
--- services/security-register/register.js
+++ services/security-register/register.js
@@ -2,8 +2,8 @@ import CredentialProvider from '../commons/remote/credentials-provider';
 import UserModel from '../commons/resources/user-model';
 
 import { getEncryptedPassword } from '../commons/helpers/password-helper';
+import { USER_STATUS, ACCOUNT_TYPE, EMAIL_STATUS } from '../commons/constants';
 
-const ACCOUNT_TYPE_BASIC = 'basic';
 const URL_USERS_API = process.env.URL_USERS_API;
 const IDENTITY_POOL_ID = process.env.IDENTITY_POOL_ID;
 const IDENTITY_ROLE_ARN = process.env.IDENTITY_ROLE_ARN;
@@ -19,25 +19,39 @@ export default function register({ username, password, fullname, genre, birthdat
   console.info('Getting user identity');
   return provider.getUserIdentity()
     .then(identityId => {
-      let user = {
-        username,
+      let userData = {
         fullname,
         genre,
         birthdate: new Date(birthdate).toISOString(),
         password: getEncryptedPassword(password),
         identity_id: identityId,
-        user_status: 'idle',
-        email_status: 'subscribed',
-        created_at: new Date().toISOString(),
+        user_status: USER_STATUS.IDLE,
+        email_status: EMAIL_STATUS.SUSCRIBED,
 
         // profile data
         space_used: 0,
-        account_type: ACCOUNT_TYPE_BASIC,
+        account_type: ACCOUNT_TYPE.BASIC,
         notifications_enabled: true
       };
 
-      console.info('Creating a new user with params: ', JSON.stringify(user, null, 2));
-      return userModel.create(user);
+      return userModel.getByUsername(username)
+        .then(user => {
+          if (user) {
+            if (user.user_status !== USER_STATUS.PENDING) {
+              throw new Error('UserAlreadyExists');
+            }
+
+            // active the pending user
+            console.info('Activating pending user : ' + user.username);
+            return userModel.update(username, userData);
+          }
+
+          userData.username = username;
+          userData.created_at = new Date().toISOString();
+          console.info('Creating a new user with params: ', JSON.stringify(user, null, 2));
+
+          return userModel.create(userData);
+        });
     })
     .catch(err => {
       console.error('An error occurred registering username ' + username + '. ' + err);
