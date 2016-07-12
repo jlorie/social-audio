c102d80663a3514d617b678320de3e34b0a397fc
Javier Lorie Guerra <jlorie87@gmail.com>
2016-07-12 15:03:52 -0500

    Implemented keep alive for element stream watcher

--- services/elements-stream-watcher/handler.js
+++ services/elements-stream-watcher/handler.js
@@ -1,6 +1,11 @@
 import { processEvent } from './process-event';
 
 export default (event, context) => {
+  if (event.keepalive) {
+    console.info('keepalive');
+    return context.succeed('keepalive');
+  }
+
   console.info('=> Input: ', JSON.stringify(event, null, 2));
 
   Promise.all(event.Records.map(processEvent))
