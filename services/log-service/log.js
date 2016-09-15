89ec4266a8d8309f19083076f46c98d7b3a6c748
pgalvezkr <pgalvez@kruger.com.ec>
2016-09-15 11:32:46 -0500

    Se modifica el output del log
    Se mejora el output del los para que sea más entendible

--- services/log-service/log.js
+++ services/log-service/log.js
@@ -1,36 +1,51 @@
 import { SUCCESS, ERR_ACTION } from '../commons/constants';
+import dynamoDoc from 'dynamo-doc';
 
 const INSERT = 'INSERT';
 const MODIFY = 'MODIFY';
 const REMOVE = 'REMOVE';
+const STAGE = process.env.SERVERLESS_STAGE;
+const REGION = process.env.SERVERLESS_REGION;
 
 export function processEvent(record) {
   let result;
+  let input = {};
+  let action = {};
+  let eventArn = record.eventSourceARN.split('table/').pop();
+  input.table = eventArn.split('/').shift();
+  input.stage = STAGE;
+  input.region = REGION;
   switch (record.eventName) {
     case INSERT:
       {
-        console.info('=== Action ===> INSERT ');
+        action.eventName = INSERT;
         result = Promise.resolve(SUCCESS);
         break;
       }
     case MODIFY:
       {
-        console.info('=== Action ===> MODIFY');
+        action.eventName = MODIFY;
         result = Promise.resolve(SUCCESS);
         break;
       }
     case REMOVE:
       {
-        console.info('=== Action ===> REMOVE');
+        action.eventName = REMOVE;
         result = Promise.resolve(SUCCESS);
         break;
       }
     default:
       {
-        console.info('=== Unfined Action ===');
+        action.eventName = 'Unfined Action';
         return Promise.resolve(ERR_ACTION.UNDEFINED);
       }
   }
+  action.NewImage = dynamoDoc.dynamoToJs(record.dynamodb.NewImage);
+  action.OldImage = dynamoDoc.dynamoToJs(record.dynamodb.OldImage);
+  input.Records = [
+    action
+  ];
+  console.info(JSON.stringify(input, null, 2));
 
   return result;
 }
