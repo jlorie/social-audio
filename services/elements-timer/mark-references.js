c6d988f21b06402eae4c3c260fab1476708962c5
Javier Lorié Guerra <jlorie87@gmail.com>
2016-09-16 19:47:41 -0400

    Corregido detalle de implemetacion

--- services/elements-timer/mark-references.js
+++ services/elements-timer/mark-references.js
@@ -5,11 +5,6 @@ const URI_ELEMENTS_BY_USERS = process.env.URI_ELEMENTS_BY_USERS;
 const elementsByUsers = new ElementUserModel(URI_ELEMENTS_BY_USERS);
 
 export default (ref) => {
-  let key = {
-    user_id: ref.user_id,
-    created_at: ref.created_at
-  };
-
-  console.info(`Updating ref ${JSON.stringify(key, null, 2)} as expired`);
-  return elementsByUsers.update(key, { ref_status: REF_STATUS.EXPIRED });
+  console.info(`Updating ref for element ${ref.id} and user ${ref.user_id} as expired`);
+  return elementsByUsers.update(ref.id, ref.user_id, { ref_status: REF_STATUS.EXPIRED });
 };
