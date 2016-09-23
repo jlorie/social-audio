bd9471cc87d8310ef46fc84ef06e8605cb9c8d18
Javier Lorié Guerra <jlorie87@gmail.com>
2016-09-23 16:47:26 -0400

    Revert "Revert "Implemented pending notification system bbluue/bbluue-client#73""
    This reverts commit 6de8025103b4b9fa0fc6514936ad11a5a74929c7.

--- services/elements-share/mark-as-resolved.js
+++ services/elements-share/mark-as-resolved.js
@@ -1,7 +1,10 @@
 import ElementUserModel from '../commons/resources/element-user-model';
+import NotificationModel from '../commons/resources/notification-model';
 import { REF_STATUS } from '../commons/constants';
 
+const URI_NOTIFICATIONS = process.env.URI_NOTIFICATIONS;
 const URI_ELEMENTS_BY_USERS = process.env.URI_ELEMENTS_BY_USERS;
+const notificationModel = new NotificationModel(URI_NOTIFICATIONS);
 const elementsByUserModel = new ElementUserModel(URI_ELEMENTS_BY_USERS);
 
 export default (element) => {
@@ -13,5 +16,26 @@ export default (element) => {
   };
 
   // update and remove expire date
-  return elementsByUserModel.rawUpdate(key, { ref_status: REF_STATUS.RESOLVED }, ['expire_at']);
+  return elementsByUserModel.rawUpdate(key, { ref_status: REF_STATUS.RESOLVED }, ['expire_at'])
+    .then(() => markNotifications(element.owner_id, element.id));
 };
+
+function markNotifications(userId, elementId) {
+  console.info('Marking notifications for element ' + elementId + ' as resolved');
+
+  return notificationModel.getPendingNotifications({ userId, elementId })
+    .then(notifications => {
+      const updateTask = (notification) => {
+        console.info('Updating notification ' + notification.id);
+        let key = {
+          user_id: notification.user_id,
+          created_at: notification.created_at
+        };
+
+        let data = { details: { pending: false } };
+        return notificationModel.updateMarkNotification(key, data);
+      };
+
+      return Promise.all(notifications.map(updateTask));
+    });
+}
