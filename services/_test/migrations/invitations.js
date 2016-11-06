8ee48f9387d36875f26729f760789a5ae6370b37
Javier Lorié Guerra <jlorie87@gmail.com>
2016-11-06 12:08:08 -0500

    Implemented invitations migration
    Invitations are extrated from Logs and persisted on invitations table

--- services/_test/migrations/invitations.js
+++ services/_test/migrations/invitations.js
@@ -1,4 +1,7 @@
+import _ from 'lodash';
 import AWS from 'aws-sdk';
+
+import ResourceModel from '../../commons/resources/resource-model';
 
 const region = process.env.SERVERLESS_REGION;
 const project = process.env.SERVERLESS_PROJECT;
@@ -14,30 +17,52 @@ export default () => {
       let tasks = logStreams.map(s => resolveLogs(s.logStreamName));
 
       return Promise.all(tasks)
-        .then(results => {
-          let processTasks = results.map(r => processEvents(r.events));
+        .then(logEvents => {
+          let tasks = logEvents.map(e => processEvents(e));
 
-          return Promise.all(processTasks);
+          return Promise.all(tasks);
         })
         .then(results => {
-          // merge sets
-          console.log('==> results: ', JSON.stringify(results));
-          // return Array.from(emails);
-          return results;
-        });
+          let set = new Set();
+          let invitations = [];
+
+          // merge maps
+          for (let map of results) {
+            for (let [email, created_at] of map) {
+              if (!set.has(email)) {
+                set.add(email);
+                invitations.push({ email, created_at });
+              }
+            }
+          }
+
+          return saveInvitations(invitations);
+        })
+        .then(() => '[Success]');
     });
 };
 
 function resolveLogStreams() {
   console.info('Resolving Log Streams ...');
   let promise = (resolve, reject) => {
-    cloudwatchLogs.describeLogStreams({ logGroupName, limit: 1 }, (err, data) => {
+    cloudwatchLogs.describeLogStreams({ logGroupName, limit: 1 }, onLogStreams);
+
+    let logStreams = [];
+
+    function onLogStreams(err, data) {
       if (err) {
         return reject(err);
       }
 
-      return resolve(data.logStreams);
-    });
+      logStreams = _.concat(logStreams, data.logStreams);
+      let nextToken = data.nextToken;
+      if (nextToken) {
+        // keep searching while nextToken is available
+        cloudwatchLogs.describeLogStreams({ logGroupName, nextToken }, onLogStreams);
+      } else {
+        return resolve(logStreams);
+      }
+    }
   };
 
   return new Promise(promise);
@@ -46,14 +71,24 @@ function resolveLogStreams() {
 function resolveLogs(logStreamName) {
   console.info(`Resolving Logs from ${logStreamName}`);
   let promise = (resolve, reject) => {
-    return cloudwatchLogs.getLogEvents({ logGroupName, logStreamName }, (err, data) => {
+    cloudwatchLogs.getLogEvents({ logGroupName, logStreamName }, onLogEvents);
+
+    let logEvents = [];
+
+    function onLogEvents(err, data) {
       if (err) {
         return reject(err);
       }
 
-      // console.info('==> data: ', JSON.stringify(data, null, 2));
-      return resolve(data);
-    });
+      logEvents = _.concat(logEvents, data.events);
+      let nextToken = (data.events.length > 0 ? data.nextBackwardToken : null);
+      if (nextToken) {
+        // keep searching while nextToken is available
+        cloudwatchLogs.getLogEvents({ logGroupName, logStreamName, nextToken }, onLogEvents);
+      } else {
+        return resolve(logEvents);
+      }
+    }
   };
 
   return new Promise(promise);
@@ -79,9 +114,14 @@ function processEvents(events) {
         emails.set(email, new Date(event.timestamp).toISOString());
       }
     }
-
-    return emails;
   }
 
   return emails;
+}
+
+function saveInvitations(invitations) {
+  console.info(`Persisting ${invitations.length} invitations`);
+
+  const invitationsModel = new ResourceModel(process.env.URI_INVITATIONS);
+  return invitationsModel.batchCreate(invitations);
 }
