601d81c4378efee2a9f20f75d471baa8a92c30a0
Javier Lorié Guerra <jlorie87@gmail.com>
2016-11-14 17:32:11 -0500

    Implemented migration for pending status in references

--- services/_migrations/migrations/fix-pending-status.js
+++ services/_migrations/migrations/fix-pending-status.js
@@ -1,6 +1,8 @@
+import _ from 'lodash';
 import ResourceModel from '../../commons/resources/resource-model';
 
 const RESOLVED_STATUS = 'resolved';
+const IDLE_STATUS = 'idle';
 const referenceModel = new ResourceModel(process.env.URI_REFERENCES);
 
 export default () => {
@@ -8,8 +10,8 @@ export default () => {
     .then(filterElements)
     .then(results => {
       console.info(`Marking ${results.length} references as idle`);
-      return 'OK';
-      // return Promise.all(results.map(ref => markRefAsPending(ref.user_id, ref.created_at)));
+      // return 'OK';
+      return Promise.all(results.map(ref => markRefAsPending(ref.user_id, ref.created_at)));
     });
 };
 
@@ -17,9 +19,10 @@ function filterElements(references) {
   let results = [];
   for (let ref of references) {
     let isOwner = ref.created_at.indexOf('owner') >= 0;
-    let hasAudio = ref.audios === undefined || ref.audios === 0;
+    let hasNoAudio = ref.audios === undefined || ref.audios === 0 ||
+      (_.isObject(ref.audios) && _.isEmpty(ref.audios));
 
-    if (isOwner && ref.ref_status !== RESOLVED_STATUS && !hasAudio) {
+    if (isOwner && ref.ref_status === RESOLVED_STATUS && hasNoAudio) {
       results.push(ref);
     }
   }
@@ -34,5 +37,5 @@ function markRefAsPending(userId, createdAt) {
   };
 
   console.info('==> Updating reference: ', JSON.stringify(key, null, 2));
-  // return resourceModel.update(key, { ref_status: IDLE_STATUS });
+  return referenceModel.update(key, { ref_status: IDLE_STATUS });
 }
