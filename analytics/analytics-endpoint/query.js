08a0d6c63097f94c0dc6b48d6175bb0bc19ed1ad
Javier Lorié Guerra <jlorie87@gmail.com>
2016-11-20 23:38:46 -0500

    Implemented endpoint for devices analytics

--- analytics/analytics-endpoint/query.js
+++ analytics/analytics-endpoint/query.js
@@ -2,12 +2,12 @@ import AnalyticsModel from '../commons/resources/analytics-model';
 
 const analyticsModel = new AnalyticsModel(process.env.ANALYTICS_URI);
 
-export function users(from, to) {
+export function queryUsers(from, to) {
   console.info(`Querying user analytics from ${from} to ${to}`);
   return analyticsModel.findByDate('user', from, to);
-};
+}
 
-export function devices(from, to) {
+export function queryDevices(from, to) {
   console.info(`Querying device analytics from ${from} to ${to}`);
-  return 'ok'; // FIXME
+  return analyticsModel.findByDate('device', from, to);
 }
